846d70104111d1d186267b3e4c2a103f000607bc
Yutaka Houlette <yutakahoulette@gmail.com>
2017-12-04 10:59:00 -0800

    Add media queries for font-sizes

--- docs/views/home/index.js
+++ docs/views/home/index.js
@@ -1,10 +1,70 @@
 const h = require('choo/html')
 const img = require('../helpers/randomImage')
 
+const navLinks = [
+  {
+    title: 'What we do',
+    url: '#'
+  },
+  {
+    title: 'Impact',
+    url: '#'
+  },
+  {
+    title: 'Who we are',
+    url: '#'
+  },
+  {
+    title: 'Insights',
+    url: '#'
+  },
+  {
+    title: 'Donate',
+    url: '#',
+    highlight: true
+  },
+  {
+    title: 'Get Updates',
+    url: '#',
+    highlight: true
+  }
+]
+
+const navLink = o => h`
+  <li>
+    <a class='color-white uppercase' href='${o.url}' title='${o.title}'>
+      ${o.title}
+    </a>
+  </li>
+`
+
+const nav = (navLinks) => h`
+  <nav>
+    <img src='./assets/svgs/logo-white-temp.svg'>
+    <ul class='list-reset m-0'>
+     ${navLinks.map(navLink)}
+    </ul>
+  </nav>
+`
+
 const home = (state) => {
   return h`
     <div>
-      <img src=${img(400, 400)}>
+      <div class='bg-cover relative bg-blue-1' style='background-image: url(${img(1000, 600)})'>
+        <div class='z-1 relative'>
+          ${nav(navLinks)}
+          <div class='py-4'>
+            <h1 class='x-large md-large sm-h1 color-white m-0'>
+              Living Goods Saves and <br>
+              Improves Lives Door-to-Door
+            </h1>
+            <div class='pt-1'>
+              <a class='btn btn--white'>Join Us</a>
+            </div>
+          </div>
+        </div>
+        <div class='bg-scrim fit-absolute'></div>
+      </div>
     </div>
   `
 }
--- docs/views/molecules/ctaBox.js
+++ docs/views/molecules/ctaBox.js
@@ -6,7 +6,7 @@ const title = 'Empowering Community Health Workers Like Janat'
 
 module.exports = h`
   <a hfre='#' class='hover-underline-parent inline-block sh-2 md-p-1 p-2 max-width-3'>
-    <h2 class='hover-underline-child serif mt-0 h1 color-blue-1'>${title}</h2>
+    <h2 class='hover-underline-child mt-0 h1 color-blue-1'>${title}</h2>
     <p class='pb-1'>${txt}</p>
     <span class='btn btn--orange mb-05'>Button</span>
   </a>
