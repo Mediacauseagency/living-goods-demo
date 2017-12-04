d6dd79949554aff0df13236f1d1b5baf829fd525
Yutaka Houlette <yutakahoulette@gmail.com>
2017-12-04 11:46:55 -0800

    Finish basic hero/nav

--- docs/views/home/index.js
+++ docs/views/home/index.js
@@ -31,18 +31,20 @@ const navLinks = [
 ]
 
 const navLink = o => h`
-  <li>
-    <a class='color-white uppercase' href='${o.url}' title='${o.title}'>
-      ${o.title}
+  <li class='px-1 sub'>
+    <a class='color-white hover-color-orange-1 uppercase border-width-1 ${o.highlight ? "border-bottom border-color-orange-1": "" }'
+       href='${o.url}' 
+       title='${o.title}'>
+        ${o.title}
     </a>
   </li>
 `
 
 const nav = (navLinks) => h`
-  <nav>
-    <img src='./assets/svgs/logo-white-temp.svg'>
-    <ul class='list-reset m-0'>
-     ${navLinks.map(navLink)}
+  <nav class='container p-1 topNav flex flex-align-center'>
+    <img class='topNav__logo' src='./assets/svgs/logo-white-temp.svg'>
+    <ul class='list-reset m-0 flex flex-justify-flex-end flex-grow-1'>
+      ${navLinks.map(navLink)}
     </ul>
   </nav>
 `
@@ -53,13 +55,15 @@ const home = (state) => {
       <div class='bg-cover relative bg-blue-1' style='background-image: url(${img(1000, 600)})'>
         <div class='z-1 relative'>
           ${nav(navLinks)}
-          <div class='py-4'>
-            <h1 class='x-large md-large sm-h1 color-white m-0'>
-              Living Goods Saves and <br>
-              Improves Lives Door-to-Door
-            </h1>
-            <div class='pt-1'>
-              <a class='btn btn--white'>Join Us</a>
+          <div class='py-4 container px-1'>
+            <div class='py-4 md-py-2 sm-py-0'>
+              <h1 class='x-large md-large sm-h1 color-white m-0'>
+                Living Goods Saves and <br>
+                Improves Lives Door-to-Door
+              </h1>
+              <div class='pt-2'>
+                <a class='btn btn--white'>Join Us</a>
+              </div>
             </div>
           </div>
         </div>
