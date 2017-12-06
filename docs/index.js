2bb469aeadc47dcd6569f45effabb1af0535d002
Yutaka Houlette <yutakahoulette@gmail.com>
2017-12-05 20:48:20 -0800

    Use settimeout to fix addSideEffects function

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,5 @@
 const h = require('choo/html')
-// const devtools = require('choo-devtools')
+const devtools = require('choo-devtools')
 const choo = require('choo')
 const Chart = require('../node_modules/chart.js')
 
@@ -7,17 +7,19 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 const views = [
   {
+    title: 'Atoms',
+    view: require('./views/atoms/index')
+  },
+  {
     title: 'Home',
     view: require('./views/home/index')
-  }
+  },
 ]
 
 const mainView = (state, emit) => {
   const tab = Number(state.query.tab || 0)
 
-  const li = (v, i) => { 
-    return '' 
-      h`
+  const li = (v, i) => h`
       <li class='inline-block m-0'>
         <a class="sub inline-block p-05 mr-05 ${tab === i ? 'border-bottom border-width-2 border-color-orange-1' : 'opacity-075'}" 
           href='?tab=${i}' 
@@ -26,7 +28,12 @@ const mainView = (state, emit) => {
         </a>
       </li>
     `
-  }
+
+  const viewWrapper = (obj, i, tab, state, emit) => h`
+    <div class='${i !== tab ? "hide" : ""}'>
+      ${obj.view(state, emit)}
+    </div>
+  `
 
   return h`
     <div>
@@ -36,7 +43,7 @@ const mainView = (state, emit) => {
         </div>
       </ul>
       <main>
-        ${views[tab].view(state, emit)}
+        ${views.map((obj, i) => viewWrapper(obj, i, tab, state, emit))}
       </main>
     </div>
   `
@@ -113,11 +120,11 @@ const toggleInView = (selector) => {
 
 const store = (state, emitter) => {
   emitter.on('DOMContentLoaded', addSideEffects)
-  emitter.on('update', addSideEffects)
+  emitter.on('render', () => window.setTimeout(addSideEffects, 100))
 }
 
 const app = choo()
-// if (!isProduction) { app.use(devtools()) }
+if (!isProduction) { app.use(devtools()) }
 app.use(store)
 app.route('*', mainView)
 app.mount('#js-container')
